6d1e4ea816ca720e2520ff4891c54e8b68afcb42
BhaskarBVV <71118230+BhaskarBVV@users.noreply.github.com>
2022-01-16 01:01:58 +0530

    Create java template 1.0 .java

---
+++ java template 1.0 .java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class editor2 {
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void solve() {
+
+    }
+
+    public static void main(String[] args) {
+
+        int n = sc.nextInt();
+        while (n-- > 0) {
+            solve();
+        }
+        sc.close();
+    }
+}
